1a6f0c4e17d75ed83ab07ff6ea0d6e4da40be2d5
Francis Yang <fy50167@gmail.com>
2024-01-15 14:18:30 -0800

    basic three.js stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
 import './App.css'
+import {useEffect} from 'react';
+import * as THREE from 'three';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth/window.innerHeight,
+      1,
+      1000
+    );
+    camera.position.z = 96;
+
+    const canvas = document.getElementById('threeJsCanvas');
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+  })
+
 
   return (
     <>
+      <div>
+        <canvas id = 'threeJsCanvas' />
+      </div>
     </>
   )
 }
